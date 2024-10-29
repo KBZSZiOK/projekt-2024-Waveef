8e20fc12e096d46b3fa9537b3046cfe767d83eef
Waveef <156914094+Waveef@users.noreply.github.com>
2024-10-29 13:56:43 +0100

    Add files via upload

---
+++ kino.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Srona główna</title>
+</head>
+<body>
+    <div id="banner"><h1>Baner</h1></div>
+    <div id="nav">
+        <a href="kino.php">Strona główna</a>
+        <a href="tabela.php">Tabele</a>
+        <a href="dodaj.php">Dodaj</a>
+    </div>
+    <div id="stopka">Stopka</div>
+</body>
