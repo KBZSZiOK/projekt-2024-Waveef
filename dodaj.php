c68d65264bb3dee6a0fb06b82c12ce41688244a0
Waveef <156914094+Waveef@users.noreply.github.com>
2024-11-07 13:36:15 +0100

    Add files via upload

--- dodaj.php
+++ dodaj.php
@@ -9,13 +9,13 @@
     <div id="nav">
         <a href="kino.php">Strona główna</a>
         <a href="tabela.php">Tabele</a>
-        <a href="dodaj.php">Dodaj</a>
+        <a href="dodaj.php">Dodaj film</a>
     </div>
     <div id="content">
         <form method="POST">
             Tytuł: <input type="text" name="tytul"><br>
             Reżyser: <input type="text" name="rezyser"><br>
-            Czas trwania<input type="text" name="czas_trwania"><br>
+            Czas trwania:<input type="text" name="czas_trwania"><br>
             <input type="submit" value="Insert">
         </form>
     </div>
--- tabela.php
+++ tabela.php
@@ -9,7 +9,7 @@
     <div id="nav">
         <a href="kino.php">Strona główna</a>
         <a href="tabela.php">Tabele</a>
-        <a href="dodaj.php">Dodaj</a>
+        <a href="dodaj.php">Dodaj film</a>
     </div>
     <div id="content">
         <table>
@@ -17,7 +17,7 @@
                 <th>ID</th>
                 <th>Nazwa</th>
                 <th>Reżyser</th>
-                <th>Czas</th>
+                <th>Czas Trwania</th>
             </tr>
             <?php
                 $sql = mysqli_connect("localhost", "root", "", "kino");
